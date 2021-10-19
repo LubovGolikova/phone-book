6e0a0927a3281331fbe3bf630184f57e2fba534e
LubovGolikova <lubovgolikova@gmail.com>
2021-10-19 18:22:15 +0300

    added sort data

--- src/components/pages/phones.js
+++ src/components/pages/phones.js
@@ -1,28 +1,42 @@
-import { Card, CardBody, CardTitle,  Button } from 'reactstrap';
+import { CardTitle,   } from 'reactstrap';
 import data from '../data/phones.json';
 import {ROUTES} from "../constants/routes";
 import {Link} from "react-router-dom";
+import AlphabetList from "react-alphabet-list";
+
 
 
 export default function Phones() {
-    const newdata = data.map((data) => {
+    const sortData =data.sort(function(a, b){
+        if(a.name.last < b.name.last) { return -1; }
+        if(a.name.last > b.name.last) { return 1; }
+        return 0;
+    });
+
+    const newdata = data.map((sortData) => {
         return (
-            <Card key={data.id}>
-                <CardBody>
-                    <CardTitle tag="h5">{data.name.last + ' '+ data.name.first }</CardTitle>
-                    <Link to={ROUTES.dynamic.details(data.id)}>
-                        <Button>details</Button>
+            <div key={sortData.id}>
+                    <Link to={ROUTES.dynamic.details(sortData.id)}>
+                        <CardTitle tag="h5">{sortData.name.last + ' '+ sortData.name.first }</CardTitle>
                     </Link>
-                </CardBody>
-            </Card>
+            </div>
         )
-    })
+    });
     return (
         <div className="container">
             <h1>Phone Book</h1>
-            {
-                newdata
-            }
+            <AlphabetList
+                className="alpha-list"
+                data={newdata}
+                generateFn={
+                    (item, index) => {
+                    return (
+                        <div key={item}>
+                            {item}
+                        </div>
+                    );
+                }}
+            />
         </div>
     );
 }
